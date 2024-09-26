f09ed1be1c8428526b0632ac5514a1b37c296352
Mikhail Lopatkin <mlopatkin@gradle.com>
2024-09-26 20:42:03 +0200

    Do not observe root project directory at configuration time in test
    This makes the whole directory an input to the configuration cache, so
    it has to be snapshotted. On Windows, snapshotting ".gradle" hits a file
    lock.

--- platforms/core-configuration/kotlin-dsl-integ-tests/src/integTest/kotlin/org/gradle/kotlin/dsl/integration/GradleKotlinDslIntegrationTest.kt
+++ platforms/core-configuration/kotlin-dsl-integ-tests/src/integTest/kotlin/org/gradle/kotlin/dsl/integration/GradleKotlinDslIntegrationTest.kt
@@ -593,7 +593,7 @@ class GradleKotlinDslIntegrationTest : AbstractKotlinIntegrationTest() {
             require(uri("settings.gradle.kts").toString().endsWith("settings.gradle.kts"), { "uri(path)" })
             require(file("settings.gradle.kts").isFile, { "file(path)" })
             require(files("settings.gradle.kts").files.isNotEmpty(), { "files(paths)" })
-            require(fileTree(".").contains(file("settings.gradle.kts")), { "fileTree(path)" })
+            require(fileTree(".") { include("*.kts") }.contains(file("settings.gradle.kts")), { "fileTree(path)" })
             require(copySpec {} != null, { "copySpec {}" })
             require(mkdir("some").isDirectory, { "mkdir(path)" })
             require(delete("some"), { "delete(path)" })
@@ -605,7 +605,7 @@ class GradleKotlinDslIntegrationTest : AbstractKotlinIntegrationTest() {
         withBuildScript(usageFor("Project"))
 
         assertThat(
-            build("help").error,
+            build("help", "--full-stacktrace").error,
             allOf(
                 containsString("Error logging from Settings"),
                 containsString("Error logging from Project")
